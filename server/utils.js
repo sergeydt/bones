213e7ecb2e40c2232083ba2c7d9ea9b8825c93cd
Kirill Hmelev <hmelev.kirill.dunice@gmail.com>
2012-08-11 16:13:38 +0400

    GUID generator helper in Bones.utils

--- server/utils.js
+++ server/utils.js
@@ -84,6 +84,16 @@ utils.sortByLoadOrder = function(assets) {
 };
 
 
+
+// Generate a pseudo-GUID by concatenating random hexadecimal.
+utils.guid = function () {
+    function S4() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+
 Error.HTTP = function(message, status) {
     if (typeof message === 'number') {
         status = message;
